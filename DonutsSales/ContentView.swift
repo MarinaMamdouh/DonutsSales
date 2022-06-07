813cab0e226251dc19af522063fc727abf179c73
MarinaMamdouh <marina.mam.riad@gmail.com>
2022-06-07 20:49:42 +0200

    Initial Commit

---
+++ DonutsSales/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  DonutsSales
+//
+//  Created by Marina on 07/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DonutsSales/DonutsSalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonutsSalesApp.swift
+//  DonutsSales
+//
+//  Created by Marina on 07/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct DonutsSalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
